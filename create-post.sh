51e980e637ccab7a752476056ffe5ca7cc1c80ee
iMocco <taoxin167@gmail.com>
2019-07-12 17:41:04 +0800

    add create post bash

---
+++ create-post.sh
@@ -0,0 +1,13 @@
+# 创建文章
+
+cd src/_posts/
+
+touch new_post.md
+
+time=$(date "+%Y-%m-%d %H:%M:%S")
+
+echo '---\ntitle: '$1'\ndate: '$time'\ncategory: \ntags: \n\t-: \nvssue-title: '$1'\n---' > $1.md
+
+echo $1'.md create success'
+
+exit
