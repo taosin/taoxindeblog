05a178da4abf82da879c51f42f57811285586910
Nix <xinxintao@foxmail.com>
2019-07-27 11:57:30 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ git commit -m 'deploy'
 
 # 推到你仓库的的 gh-page 分支
 # 将 <USERNAME>/<REPO> 替换为你的信息
-git push -f git@git.dev.tencent.com:imocco/imocco.coding.me.git master
+git push -f https://git.dev.tencent.com/imocco/imocco.coding.me.git master
